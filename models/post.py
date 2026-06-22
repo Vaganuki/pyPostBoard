096695bcb7d7b357c36cd5e507e5dfeb02a4297f
Vaganuki <hugo.warnotte@gmail.com>
2026-06-22 14:20:20 +0200

    post init

--- models/post.py
+++ models/post.py
@@ -1,7 +1,12 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
 from .base import Base
 
 
 class Post(Base):
     __tablename__ = "post"
-
+    id : Mapped[int] = mapped_column(primary_key=True,autoincrement=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    content: Mapped[str] = mapped_column(nullable=False)
+    img_url: Mapped[str] = mapped_column()
     pass
